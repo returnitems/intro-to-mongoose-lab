234311fe7e6a2cd7af29b37efd6713437d8f24b8
diwanumang <diwan.umang@gmail.com>
2024-04-27 23:05:18 -0400

    Swapped if/else statements with switch/case after learning about them. Working on update and delete part of CRUD

--- app.js
+++ app.js
@@ -10,6 +10,69 @@ const connect = async () => {
   await mongoose.connect(process.env.MONGODB_URI);
 };
 
+const disconnect = async () => {
+  await mongoose.disconnect(process.env.MONGODB_URI);
+};
+
+// const createCustomer = async () => {
+//   const customerData = {
+//     name: custName,
+//     age: custAge,
+//   };
+
+//   const Customer = await customer.create(customerData);
+//   console.log("New customer:", Customer);
+// };
+
+// const custQueries = async () => {
+//   console.log("Queries running");
+//   await createCustomer();
+
+// };
+
+// const findCustomers = async () => {
+//   const cust = await customer.find({});
+//   console.log("Below is the list of all customers:");
+//   cust.forEach((e) => {
+//     console.log(`id: ${e._id} -- Name: ${e.name}, Age: ${e.age}`);
+//   });
+//   let continueMenu = prompt("Type 1 and press enter to return to main menu: ");
+// };
+
+// const mainMenu = `Welcome to the CRM
+
+// What would you like to do?
+
+//     1. Create a customer
+//     2. View all customers
+//     3. Update a customer
+//     4. Delete a customer
+//     5. Quit
+//     `;
+
+// console.clear();
+
+// console.log(mainMenu);
+// let answer = prompt("Type the number of the action to run: ");
+
+// if (answer === "1") {
+//   console.clear();
+//   console.log("Great! Lets enter a new customer in our DB.");
+//   custName = prompt("Please type the name of the customer: ");
+//   custAge = prompt("Please type the age of the customer: ");
+//   custQueries();
+// } else if (answer === "2") {
+//   console.clear();
+//   findCustomers();
+// } else if (answer === "3") {
+//   console.clear();
+//   updateCustomer();
+// }
+
+// connect();
+
+// ************************************************************************************************
+
 const createCustomer = async () => {
   const customerData = {
     name: custName,
@@ -20,25 +83,24 @@ const createCustomer = async () => {
   console.log("New customer:", Customer);
 };
 
-const custQueries = async () => {
-  console.log("Queries running");
-  await createCustomer();
-  // Option 2 printing all customers
-  //   await findCustomers();
-};
-
 const findCustomers = async () => {
   const cust = await customer.find({});
   console.log("Below is the list of all customers:");
   cust.forEach((e) => {
     console.log(`id: ${e._id} -- Name: ${e.name}, Age: ${e.age}`);
   });
-  let continueMenu = prompt("Type 1 and press enter to return to main menu: ");
+  // let continueMenu = prompt("Type 1 and press enter to return to main menu: ");
 };
 
-const mainMenu = `Welcome to the CRM
+const mainMenu = async () => {
+  await connect();
+  console.clear();
+  console.log("Welcome to the CRM!");
+  let running = true;
+  while (running) {
+    // console.clear();
 
-What would you like to do?
+    const menuText = `What would you like to do?
 
     1. Create a customer
     2. View all customers
@@ -47,23 +109,53 @@ What would you like to do?
     5. Quit
     `;
 
-console.clear();
+    console.log(menuText);
+    let answer = prompt("Type the number of the action to run: ");
 
-console.log(mainMenu);
-let answer = prompt("Type the number of the action to run: ");
+    // if (answer === "1") {
+    //   console.clear();
+    //   console.log("Great! Lets enter a new customer in our DB.");
+    //   custName = prompt("Please type the name of the customer: ");
+    //   custAge = prompt("Please type the age of the customer: ");
+    //   createCustomer();
+    // } else if (answer === "2") {
+    //   console.clear();
+    //   findCustomers();
+    // } else if (answer === "3") {
+    //   console.clear();
+    //   updateCustomer();
+    // } else if (answer === "4") {
+    //   console.clear();
+    // } else if (answer === "5") {
+    //   disconnect();
+    // }
 
-if (answer === "1") {
-  console.clear();
-  console.log("Great! Lets enter a new customer in our DB.");
-  custName = prompt("Please type the name of the customer: ");
-  custAge = prompt("Please type the age of the customer: ");
-  custQueries();
-} else if (answer === "2") {
-  console.clear();
-  findCustomers();
-} else if (answer === "3") {
-  console.clear();
-  updateCustomer();
-}
+    switch (answer) {
+      case "1":
+        console.clear();
+        console.log("Great! Lets enter a new customer in our DB.");
+        custName = prompt("Please type the name of the customer: ");
+        custAge = prompt("Please type the age of the customer: ");
+        await createCustomer();
+        break;
+      case '2':
+        console.clear();
+        await findCustomers();
+        break;
+      case '3':
+        console.clear();
+        await updateCustomer();
+        break;
+      case '4':
+        console.clear();
+        await deleteCustomer();
+        break;
+      case '5':
+        await disconnect();
+        running = false;
+        break;
+    }
+  }
+};
 
-connect();
+mainMenu();
